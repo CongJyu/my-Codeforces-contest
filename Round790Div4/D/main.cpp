727c7ba250b54abae09f1fbbcf63d779190887fe
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-05-14 21:26:09 +0800

    Update round790div4

--- Round790Div4/D/main.cpp
+++ Round790Div4/D/main.cpp
@@ -1,6 +1,7 @@
 // problem d
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -19,7 +20,39 @@ int main() {
                 cin >> board[i][j];
             }
         }
-        
+        int max_v = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                int now = 0;
+                int ci = i, cj = j;
+                while (ci >= 0 && ci < n && cj >= 0 && cj < m) {
+                    now += board[ci][cj];  // left-up
+                    ci--;
+                    cj--;
+                }
+                ci = i, cj = j;
+                while (ci >= 0 && ci < n && cj >= 0 && cj < m) {
+                    now += board[ci][cj];  // right-up
+                    ci++;
+                    cj--;
+                }
+                ci = i, cj = j;
+                while (ci >= 0 && ci < n && cj >= 0 && cj < m) {
+                    now += board[ci][cj];  // left-down
+                    ci--;
+                    cj++;
+                }
+                ci = i, cj = j;
+                while (ci >= 0 && ci < n && cj >= 0 && cj < m) {
+                    now += board[ci][cj];  // right-down
+                    ci++;
+                    cj++;
+                }
+                now -= board[i][j] * 3;
+                max_v = max(max_v, now);
+            }
+        }
+        cout << max_v << endl;
     }
     return 0;
 }
