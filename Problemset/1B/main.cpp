256119fce089231922a3c2f4a7a2194e680dc1c8
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-07-05 10:20:13 +0800

    add cf1a

---
+++ Problemset/1B/main.cpp
@@ -0,0 +1,18 @@
+// cf 1b
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n = 0;
+    cin >> n;
+    while (n--) {
+        string in;
+        cin >> in;
+        
+    }
+    return 0;
+}
