de4ca85a6ba7e9d78a0b426d07de698a9a7a2b71
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-07-19 23:21:01 +0800

    add 1703c

---
+++ 1703C/main.cpp
@@ -0,0 +1,19 @@
+// codeforces 1703c
+
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int t{};
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+    return 0;
+}
