c8bb82bf93a52aeed68d3cddf64dc510eee98540
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-07-19 17:41:52 +0800

    add 1a

---
+++ 1A/main.cpp
@@ -0,0 +1,24 @@
+// codeforces 1a
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    long long n{}, m{}, a{};
+    cin >> n >> m >> a;
+    if (n % a == 0) {
+        n /= a;
+    } else {
+        n = n / a + 1;
+    }
+    if (m % a == 0) {
+        m /= a;
+    } else {
+        m = m / a + 1;
+    }
+    long long result{n * m};
+    cout << result << endl;
+    return 0;
+}
