3d2878349300f68fd2a8d9a911b964715e0d1f98
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-07-20 16:09:43 +0800

    add 1665a

---
+++ 1665A/main.cpp
@@ -0,0 +1,17 @@
+// codeforces 1665a
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int t{};
+    cin >> t;
+    while (t--) {
+        int n{};
+        cin >> n;
+        cout << n - 3 << " " << 1 << " " << 1 << " " << 1 << endl;
+    }
+    return 0;
+}
