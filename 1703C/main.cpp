7c04b21534765619058eb1773ef7431f104adaaa
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-07-20 09:57:55 +0800

    add 1703c

--- 1703C/main.cpp
+++ 1703C/main.cpp
@@ -5,7 +5,35 @@
 using namespace std;
 
 void solution() {
-
+    int n{};
+    cin >> n;
+    int *a{new int[n]};
+    for (int i{}; i < n; ++i) {
+        cin >> a[i];
+    }
+    for (int i{}; i < n; ++i) {
+        int num{};
+        cin >> num;
+        string opt;
+        cin >> opt;
+        for (int j{}; j < (int) opt.length(); ++j) {
+            if (opt[j] == 'U') {
+                a[i]--;
+                if (a[i] < 0) {
+                    a[i] += 10;
+                }
+            } else {
+                a[i]++;
+                if (a[i] > 9) {
+                    a[i] -= 10;
+                }
+            }
+        }
+    }
+    for (int i{}; i < n; ++i) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
 }
 
 int main() {
