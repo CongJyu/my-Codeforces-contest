9e84676bc0a915a3a5c8e9512725e55e8ecddb4b
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-07 12:09:22 +0800

    add 158a

--- 158A/main.cpp
+++ 158A/main.cpp
@@ -6,6 +6,18 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    
+    int n{}, k{};
+    cin >> n >> k;
+    int *a{new int[n]}, cnt{};
+    for (int i{}; i < n; ++i) {
+        cin >> a[i];
+    }
+    for (int i{}; i < n; ++i) {
+        if (a[i] >= a[k - 1] and a[i] > 0) {
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+    delete[] a;
     return 0;
 }
