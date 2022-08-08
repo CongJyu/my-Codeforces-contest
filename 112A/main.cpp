c90553dcbbf3dc4151ac1c5b219686b2a054a16d
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-08 17:01:55 +0800

    add 476a and 112a

---
+++ 112A/main.cpp
@@ -0,0 +1,23 @@
+// codeforces 112a
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    string s1, s2;
+    cin >> s1 >> s2;
+    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+    transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+    if (s1 > s2) {
+        cout << 1 << endl;
+    } else if (s1 == s2) {
+        cout << 0 << endl;
+    } else {
+        cout << -1 << endl;
+    }
+    return 0;
+}
---
+++ 476A/main.cpp
@@ -0,0 +1,13 @@
+// codeforces 476a
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n{}, m{};
+    cin >> n >> m;
+    
+    return 0;
+}
