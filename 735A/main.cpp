b2851372ac3957386e9521974a8c5e8e75122749
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-09 17:33:23 +0800

    add 735a

---
+++ 735A/main.cpp
@@ -0,0 +1,15 @@
+// codeforces 735a
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int n{}, k{};
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    // no idea
+    return 0;
+}
