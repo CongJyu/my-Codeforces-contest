89958fc4ba3358b044c3c23cf7d773fa9a976333
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-19 16:15:56 +0800

    add 1720c

--- 1720C/main.cpp
+++ 1720C/main.cpp
@@ -1,54 +1,102 @@
 // codeforces 1720c
 
+//#include <iostream>
+//
+//using namespace std;
+//
+///*
+// * a[i][j]      a[i][j + 1]
+// * a[i + 1][j]  a[i + 1][j + 1]
+// */
+//
+//void solution() {
+//    int n{}, m{};
+//    cin >> n >> m;
+//    char a[501][501]{'0'};
+//    for (int i{}; i < n; ++i) {
+//        for (int j{}; j < m; ++j) {
+//            cin >> a[i][j];
+//        }
+//    }
+//    int cnt{};
+//    for (int i{}; i < n - 1; ++i) {
+//        for (int j{}; j < m - 1; ++j) {
+//            if (a[i][j] - '0' + a[i][j + 1] - '0' + a[i + 1][j] - '0' + a[i + 1][j + 1] - '0' == 4) {
+//                cnt += 2;
+//                a[i][j] = '0';
+//                a[i][j + 1] = '0';
+//                a[i + 1][j] = '0';
+//                a[i + 1][j + 1] = '0';
+//            } else if (a[i][j] - '0' + a[i][j + 1] - '0' + a[i + 1][j] - '0' + a[i + 1][j + 1] - '0' == 3) {
+//                cnt += 2;
+//                a[i][j] = '0';
+//                a[i][j + 1] = '0';
+//                a[i + 1][j] = '0';
+//                a[i + 1][j + 1] = '0';
+//            } else if (a[i][j] - '0' + a[i][j + 1] - '0' + a[i + 1][j] - '0' + a[i + 1][j + 1] - '0' == 2) {
+//                cnt += 2;
+//                a[i][j] = '0';
+//                a[i][j + 1] = '0';
+//                a[i + 1][j] = '0';
+//                a[i + 1][j + 1] = '0';
+//            } else if (a[i][j] - '0' + a[i][j + 1] - '0' + a[i + 1][j] - '0' + a[i + 1][j + 1] - '0' == 1) {
+//                cnt++;
+//                a[i][j] = '0';
+//                a[i][j + 1] = '0';
+//                a[i + 1][j] = '0';
+//                a[i + 1][j + 1] = '0';
+//            }
+//        }
+//    }
+//    cout << cnt << endl;
+//}
+//
+//int main() {
+//    ios::sync_with_stdio(false);
+//    int t{};
+//    cin >> t;
+//    while (t--) {
+//        solution();
+//    }
+//    return 0;
+//}
+
+
 #include <iostream>
 
 using namespace std;
 
-/*
- * a[i][j]      a[i][j + 1]
- * a[i + 1][j]  a[i + 1][j + 1]
- */
+const int MAXN{2000};
+
+int a[MAXN][MAXN]{0};
 
 void solution() {
     int n{}, m{};
     cin >> n >> m;
-    char a[501][501]{'0'};
+    int sum{};
+    string s;
     for (int i{}; i < n; ++i) {
+        cin >> s;
         for (int j{}; j < m; ++j) {
-            cin >> a[i][j];
+            a[i][j] = s[j] - '0';
+            sum += a[i][j];
         }
     }
-    int cnt{};
+    int minn{MAXN};
     for (int i{}; i < n - 1; ++i) {
         for (int j{}; j < m - 1; ++j) {
-            if (a[i][j] - '0' + a[i][j + 1] - '0' + a[i + 1][j] - '0' + a[i + 1][j + 1] - '0' == 4) {
-                cnt += 2;
-                a[i][j] = '0';
-                a[i][j + 1] = '0';
-                a[i + 1][j] = '0';
-                a[i + 1][j + 1] = '0';
-            } else if (a[i][j] - '0' + a[i][j + 1] - '0' + a[i + 1][j] - '0' + a[i + 1][j + 1] - '0' == 3) {
-                cnt += 2;
-                a[i][j] = '0';
-                a[i][j + 1] = '0';
-                a[i + 1][j] = '0';
-                a[i + 1][j + 1] = '0';
-            } else if (a[i][j] - '0' + a[i][j + 1] - '0' + a[i + 1][j] - '0' + a[i + 1][j + 1] - '0' == 2) {
-                cnt += 2;
-                a[i][j] = '0';
-                a[i][j + 1] = '0';
-                a[i + 1][j] = '0';
-                a[i + 1][j + 1] = '0';
-            } else if (a[i][j] - '0' + a[i][j + 1] - '0' + a[i + 1][j] - '0' + a[i + 1][j + 1] - '0' == 1) {
-                cnt++;
-                a[i][j] = '0';
-                a[i][j + 1] = '0';
-                a[i + 1][j] = '0';
-                a[i + 1][j + 1] = '0';
+            int cnt{a[i][j] + a[i + 1][j] + a[i][j + 1] + a[i + 1][j + 1]};
+            if (cnt == 0) {
+                continue;
             }
+            minn = min(minn, max(1, cnt - 1));
         }
     }
-    cout << cnt << endl;
+    if (sum == 0) {
+        cout << 0 << endl;
+    } else {
+        cout << 1 + sum - minn << endl;
+    }
 }
 
 int main() {
