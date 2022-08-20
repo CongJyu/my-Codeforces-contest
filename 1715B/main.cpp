f4aa6b301e53e0512b518e5c7d2bedffe6aec8a5
Rain Chen <87171700+Chen-Rain@users.noreply.github.com>
2022-08-20 22:55:49 +0800

    add 1715b

---
+++ 1715B/main.cpp
@@ -0,0 +1,22 @@
+// codeforces 1715b
+
+#include <iostream>
+
+using namespace std;
+
+void solution() {
+
+    long long n{}, k{}, b{}, s{};
+    cin >> n >> k >> b >> s;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int t{};
+    cin >> t;
+    while (t--) {
+        solution();
+    }
+    return 0;
+}
